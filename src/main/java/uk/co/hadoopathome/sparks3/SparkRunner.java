1eac178dc695e07bf37abbcd613143079b761234
Ben Watson <benwatson528@gmail.com>
2020-05-05 21:55:59 +0100

    Cleaning local test

--- src/main/java/uk/co/hadoopathome/sparks3/SparkRunner.java
+++ src/main/java/uk/co/hadoopathome/sparks3/SparkRunner.java
@@ -1,11 +1,9 @@
 package uk.co.hadoopathome.sparks3;
 
 import org.apache.spark.sql.SparkSession;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class SparkRunner {
-  
+
   public static void processSpark(String[] args, SparkSession sparkSession) {
     String inputFile = args[0];
     String outputDir = args[1];
